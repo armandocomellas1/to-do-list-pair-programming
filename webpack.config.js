f33afce58684a5cc4b387fdac234a6f65a54ea02
GameMaster <businesswitharmando@gmail.com>
2022-08-19 17:58:50 -0500

    I have added all the necessary packages and modules in order to run this

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,6 @@ module.exports = {
     clean: true,
   },
   module: {
-    loaders: [
-      { exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/ },
-      { loader: 'style-loader!css-loader', test: /\.css$/ },
-      { loader: 'url-loader', test: /\.gif$/ },
-      { loader: 'file-loader', test: /\.(ttf|eot|svg)$/ },
-    ],
     rules: [
       {
         test: /\.css$/i,
@@ -39,12 +33,6 @@ module.exports = {
     runtimeChunk: 'single',
   },
   resolve: {
-    extensions: ['', 'js', 'jsx'],
-    modules: [
-      'node_modules',
-      'bower_components',
-      'shared',
-      '/shared/vendor/modules',
-    ],
+    extensions: ['.js'],
   },
 };
