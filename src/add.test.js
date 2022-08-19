2a872684577b8616ab92729fff44c8e0092bf2d1
GameMaster <businesswitharmando@gmail.com>
2022-08-19 17:57:34 -0500

    I have added the clean code for testing the two function add and deelete

--- src/add.test.js
+++ src/add.test.js
@@ -1,5 +1,43 @@
-const addTask = require('./index');
+/**
+ * @jest-environment jsdom
+ */
+import addLine from './addItem';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(addTask()).toBe(3);
+window.localStorage = Storage.prototype;
+document.body.innerHTML = `
+<div class="container">
+  <div class="cont_title">
+    <div class="title">Today's To Do</div>
+    <div class="img_refresh"></div>
+  </div>
+  <div class="add_list">
+    <input class="input_two" type="text" placeholder="Add to your list..." value="">
+    <div class="img_list"></div>
+  </div>
+  <div id="list_row" class="contains"></div>
+  <button id="unique" class="footer" type="reset">Clear all completed</button>
+</div>
+`;
+const getContainer = document.getElementById('list_row');
+const getOBject = addLine.addItem('Finish Challenge');
+getContainer.appendChild(getOBject);
+const getCount = getOBject.index;
+const getNameInput = document.getElementsByClassName('editable')[0].innerHTML;
+const getObjectProp = getOBject.classList.value;
+
+describe('Test add and remove', () => {
+  test('Expect input text should not be empty', () => {
+    const validateInput = addLine.addItem(getNameInput);
+    expect(validateInput).not.toBe('');
+  });
+
+  test('Expect object of list has not index of cero', () => {
+    const validateInput = addLine.addItem(getCount);
+    expect(validateInput).not.toBe(0);
+  });
+
+  test('Expect to remove exactly the line', () => {
+    const validateInput = addLine.deleteElement(getObjectProp);
+    expect(validateInput).toBeUndefined();
+  });
 });
